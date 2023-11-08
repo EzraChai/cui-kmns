20a0d5cbbe3ea5279344df6e7a3a84391e20d59d
Ezra Chai <juanzhe2@gmail.com>
2023-11-08 23:53:23 +0800

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,11 @@ export default async function Home() {
           <div className={mashanzheng.className}>
             <h1 className="text-center text-[14rem] lg:text-[28rem]">脆</h1>
           </div>
-          <div className="col-span-2 flex  items-center ">
-            <p className=" lg:w-4/5 mx-auto text-lg lg:text-xl text-neutral-600 dark:text-neutral-400">
+          <div className="col-span-2 items-center ">
+            <h3 className={`${mashanzheng.className} text-2xl lg:text-5xl`}>
+              历史
+            </h3>
+            <p className="mt-4 lg:mt-8 mx-auto text-md lg:text-md text-neutral-600 dark:text-neutral-400">
               &quot;脆&quot;文化起源于2023年7月10日，由麦索阁同志启发。初始成员约15位
               first intake 的KMNS同志组成。过后由 second intake 和 third intake
               的同志组成一个大约50位属于男人的团体。
@@ -30,7 +33,7 @@ export default async function Home() {
           <h3 className={`${mashanzheng.className} text-2xl lg:text-5xl`}>
             成员介绍
           </h3>
-          <div className="mt-4 lg:mt-12 grid grid-cols-2 lg:grid-cols-5 gap-4">
+          <div className="mt-4 lg:mt-8 grid grid-cols-2 lg:grid-cols-5 gap-4">
             {members.map((member: Member) => (
               <MemberCard key={member._id} member={member} />
             ))}
