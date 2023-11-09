671e79278b238def1f11a30d7e947f161043a52e
Ezra Chai <juanzhe2@gmail.com>
2023-11-09 23:54:51 +0800

    fixed bug

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -29,7 +29,11 @@ export default function MemberCard({ member }: { member: Member }) {
               className="border-2 border-black dark:border-white rounded-lg"
               width={200}
               height={200}
-              src={urlFor(member.profileImage).width(200).height(200).url()}
+              src={urlFor(member.profileImage)
+                .width(200)
+                .height(200)
+                .quality(40)
+                .url()}
               alt={`Profile Image for ${member.englishName}`}
             />
           </div>
@@ -37,7 +41,7 @@ export default function MemberCard({ member }: { member: Member }) {
           <CardContent>
             <CardTitle>{member.chineseName}</CardTitle>
             <CardDescription className="capitalize">
-              <p>{member.englishName}</p>
+              {member.englishName}
             </CardDescription>
             <div className="flex justify-center lg:flex-wrap flex-wrap flex-grow-0 gap-1 mt-4">
               {member.tags.slice(0, 2).map((tag) => (
@@ -59,11 +63,11 @@ export default function MemberCard({ member }: { member: Member }) {
               <div className="flex lg:block justify-center items-center">
                 <Image
                   className="border-2 border-black dark:border-white rounded-lg"
-                  width={400}
-                  height={400}
+                  width={300}
+                  height={300}
                   src={urlFor(member.profileImage)
-                    .width(400)
-                    .height(400)
+                    .width(300)
+                    .height(300)
                     .auto("format")
                     .url()}
                   alt={`Profile Image for ${member.englishName}`}
@@ -71,12 +75,12 @@ export default function MemberCard({ member }: { member: Member }) {
               </div>
               <div className=" mt-12 lg:mt-0 relative">
                 <Label htmlFor="chineseName">名字</Label>
-                <p
+                <div
                   className="text-black capitalize dark:text-white mb-2 text-xl font-semibold"
                   id="chineseName"
                 >
                   {member.chineseName} {member.englishName}
-                </p>
+                </div>
 
                 <Label htmlFor="from">来自</Label>
                 <p
@@ -87,7 +91,7 @@ export default function MemberCard({ member }: { member: Member }) {
                 </p>
 
                 <Label htmlFor="from">我是</Label>
-                <p className="flex mt-1 gap-1 dark:text-white text-black mb-4">
+                <div className="flex justify-center lg:justify-start mt-1 gap-1 dark:text-white text-black mb-4">
                   {member.tags.map((tag) => (
                     <div
                       key={tag._key}
@@ -96,7 +100,7 @@ export default function MemberCard({ member }: { member: Member }) {
                       {tag.tag}
                     </div>
                   ))}
-                </p>
+                </div>
 
                 <Label htmlFor="from">自我介绍</Label>
                 <p className="mb-16 dark:text-white text-black">
