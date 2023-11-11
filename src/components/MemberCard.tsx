2227f2802140a283bfb02c926d1a94b4983a3220
Ezra Chai <juanzhe2@gmail.com>
2023-11-11 11:19:59 +0800

    update scroll area

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -18,6 +18,7 @@ import { Label } from "@/components/ui/label";
 import { urlFor } from "@/lib/client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "./ui/scroll-area";
 
 function shortenName(name: string) {
   let nameArray = name.trim().split(" ");
@@ -54,73 +55,76 @@ export default function MemberCard({ member }: { member: Member }) {
           </CardContent>
         </Card>
       </DialogTrigger>
+
       <DialogContent className="w-[90%] lg:w-full">
-        <DialogHeader>
-          <DialogDescription>
-            <div className="grid grid-cols-1 lg:gap-4 lg:grid-cols-2">
-              <div className="flex lg:block justify-center items-center">
-                <Image
-                  className="mt-2 lg:mt-0 border-2 border-black dark:border-white rounded-lg"
-                  width={250}
-                  height={250}
-                  src={urlFor(member.profileImage)
-                    .width(300)
-                    .height(300)
-                    .auto("format")
-                    .url()}
-                  alt={`Profile Image for ${member.englishName}`}
-                />
+        <ScrollArea className="h-[580px] lg:h-full w-full">
+          <DialogHeader>
+            <DialogDescription>
+              <div className="grid grid-cols-1 lg:gap-4 lg:grid-cols-2">
+                <div className="flex lg:block justify-center items-center">
+                  <Image
+                    className="mt-2 lg:mt-0 border-2 border-black dark:border-white rounded-lg"
+                    width={250}
+                    height={250}
+                    src={urlFor(member.profileImage)
+                      .width(300)
+                      .height(300)
+                      .auto("format")
+                      .url()}
+                    alt={`Profile Image for ${member.englishName}`}
+                  />
+                </div>
+                <div className=" mt-12 lg:mt-0 relative">
+                  <Label htmlFor="chineseName">名字</Label>
+                  <div
+                    className="text-black capitalize dark:text-white mb-2 text-xl font-semibold"
+                    id="chineseName"
+                  >
+                    {member.chineseName} {member.englishName}
+                  </div>
+
+                  <Label htmlFor="from">来自</Label>
+                  <p
+                    className="text-black mb-2 dark:text-white text-lg font-semibold"
+                    id="from"
+                  >
+                    {member.hometown}
+                  </p>
+
+                  <Label htmlFor="from">特色</Label>
+                  <div className="flex justify-center lg:justify-start mt-1 gap-1 dark:text-white text-black mb-4">
+                    {member.tags.map((tag) => (
+                      <div
+                        key={tag._key}
+                        className="border-black border-2 dark:border-white text-xs font-extrabold px-2 py-1 rounded-lg"
+                      >
+                        {tag.tag}
+                      </div>
+                    ))}
+                  </div>
+
+                  <Label htmlFor="from">自我介绍</Label>
+                  <p className="mb-16 dark:text-white text-black">
+                    {member.description}
+                  </p>
+
+                  <div className="absolute bottom-0 flex items-center">
+                    <Instagram />
+                    <Button variant={"link"}>
+                      <a
+                        referrerPolicy="no-referrer"
+                        target="_blank"
+                        href={`https://www.instagram.com/${member.instagramAccount}`}
+                      >
+                        @{member.instagramAccount}
+                      </a>
+                    </Button>
+                  </div>
+                </div>
               </div>
-              <div className=" mt-12 lg:mt-0 relative">
-                <Label htmlFor="chineseName">名字</Label>
-                <div
-                  className="text-black capitalize dark:text-white mb-2 text-xl font-semibold"
-                  id="chineseName"
-                >
-                  {member.chineseName} {member.englishName}
-                </div>
-
-                <Label htmlFor="from">来自</Label>
-                <p
-                  className="text-black mb-2 dark:text-white text-lg font-semibold"
-                  id="from"
-                >
-                  {member.hometown}
-                </p>
-
-                <Label htmlFor="from">特色</Label>
-                <div className="flex justify-center lg:justify-start mt-1 gap-1 dark:text-white text-black mb-4">
-                  {member.tags.map((tag) => (
-                    <div
-                      key={tag._key}
-                      className="border-black border-2 dark:border-white text-xs font-extrabold px-2 py-1 rounded-lg"
-                    >
-                      {tag.tag}
-                    </div>
-                  ))}
-                </div>
-
-                <Label htmlFor="from">自我介绍</Label>
-                <p className="mb-16 dark:text-white text-black">
-                  {member.description}
-                </p>
-
-                <div className="absolute bottom-0 flex items-center">
-                  <Instagram />
-                  <Button variant={"link"}>
-                    <a
-                      referrerPolicy="no-referrer"
-                      target="_blank"
-                      href={`https://www.instagram.com/${member.instagramAccount}`}
-                    >
-                      @{member.instagramAccount}
-                    </a>
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </DialogDescription>
-        </DialogHeader>
+            </DialogDescription>
+          </DialogHeader>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
