4d6f4109958651e652a64fd501df0f4e2af352ac
Ezra Chai <juanzhe2@gmail.com>
2023-11-11 18:37:07 +0800

    update date

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,7 +5,7 @@ import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // "pv8y60vp"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-11-13";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-20";
 
 export const client = createClient({
   projectId,
