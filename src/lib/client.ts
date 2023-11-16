fae8072ba5268d1cfa49d0e90072c96379c5c414
Ezra Chai <juanzhe2@gmail.com>
2023-11-16 17:24:32 +0800

    update

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,13 +5,13 @@ import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // "pv8y60vp"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-08-15";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-08-16";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion, // https://www.sanity.io/docs/api-versioning
-  useCdn: false, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+  useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 });
 
 const builder = imageUrlBuilder(client);
