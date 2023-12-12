c2625eecba961cb108c402d175a718377e328312
Ezra Chai <juanzhe2@gmail.com>
2023-12-12 20:49:27 +0800

    update target

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <div className="mt-24 mb-4 flex justify-center gap-4 text-neutral-600 dark:text-neutral-400 max-w-7xl px-4 font-sm text-xs mx-auto text-center">
       <div className="">©2023 脆</div>
       <a
+        target="_blank"
         className="hover:text-white transition"
         referrerPolicy="no-referrer"
         href="https://www.instagram.com/cui_brittle2324/"
