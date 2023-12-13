77b2c329b4046eb077b794cfa927efe79b0e6798
Ezra Chai <juanzhe2@gmail.com>
2023-12-13 15:09:17 +0800

    update logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { mashanzheng } from "@/app/layout";
 import { ModeToggle } from "@/components/darkmode-button";
 import { Separator } from "@/components/ui/separator";
+import CuiLogo from "../../public/cui-logo.png";
+import Image from "next/image";
 
 export function Navbar() {
   return (
@@ -8,7 +10,12 @@ export function Navbar() {
       <div className="max-w-5xl py-2 mx-auto px-4 md:px-6 lg:px-0">
         <ul className="flex justify-between items-center">
           <li className={mashanzheng.className}>
-            <h1 className="text-[2rem] lg:text-[2.50rem]">脆</h1>
+            <Image
+              className="w-14 h-14 lg:w-16 lg:h-16 dark:invert-0 invert"
+              src={CuiLogo}
+              alt="Cui's Logo"
+            />
+            {/* <h1 className="text-[2rem] lg:text-[2.50rem]">脆</h1> */}
           </li>
           <li>
             <ModeToggle />
